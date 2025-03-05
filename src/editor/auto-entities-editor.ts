2efa3e75192a6ce38fabdc28fbe6bca56bd3fe35
Naomi Rennie-Waldock <naomi.renniewaldock@gmail.com>
2025-03-05 22:46:16 +0000

    Fix filters UI (fixes #481, #483, #491)

--- src/editor/auto-entities-editor.ts
+++ src/editor/auto-entities-editor.ts
@@ -121,7 +121,8 @@ class AutoEntitiesEditor extends LitElement {
     const data = form2filter(this._config, ev.detail.value);
     const include = [...this._config.filter?.include];
     include[group] = { ...data, options: include[group].options };
-    this._config.filter = { ...this._config.filter, include };
+    const filter = { ...this._config.filter, include };
+    this._config = { ...this._config, filter };
     this.dispatchEvent(
       new CustomEvent("config-changed", { detail: { config: this._config } })
     );
