9877e91963811e5afff39d9678afcc1f3103d07e
Thomas Lovén <thomasloven@gmail.com>
2025-05-28 13:10:10 +0000

    Re-fix #543

--- src/editor/auto-entities-editor.ts
+++ src/editor/auto-entities-editor.ts
@@ -12,7 +12,7 @@ customElements.whenDefined("ha-yaml-editor").then(() => {
   const HaYamlEditor = customElements.get("ha-yaml-editor").prototype;
   const orig_setValue = HaYamlEditor.setValue;
   HaYamlEditor.setValue = function (value) {
-    if (!this._yaml || !compare_deep(value, this.value))
+    if (this.autoUpdate || !compare_deep(value, this.value))
       orig_setValue.bind(this)(value);
   };
 });
